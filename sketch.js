44ef7ed4134989bbd46766970ad77e6080aa81b4
ecarpio23 <124626883+ecarpio23@users.noreply.github.com>
2023-04-17 14:45:30 -0400

    Commit 1

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+//this function creates the breed list from the api (Gets the info)
+//async function start() {
+//  const response  = await fetch("https://dog.ceo/api/breeds/list/all")
+  //const data = await response.json()
+ // createBreedList(data.message)}
+
+//start()
+//this function gives the breed choices
+//function createBreedList(breedList) {
+//document.getElementById("breed").innerHTML = `
+//<select onchange="loadByBreed(this.value)">
+           // <option>Choose a dog breed</option>
+           // ${Object.keys(breedList).map(function (breed) {
+          //    return `<option>${breed}</option>`
+          //  }).join('')}
+     //   </select>`}
+
+//this function loads the info for each breed
+//async function loadByBreed(breed) {
+  //if statement so that the holder text doesn't get used as a breed
+  //if (breed != "Choose a dog breed") {
+    //const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+   // const data = await response.json()
+    //console.log(data.message) }}
